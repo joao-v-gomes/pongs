97a348a496270e14732af2af3058a1bea0ec56a5
Joao Gomes <joao.victor214@gmail.com>
2022-06-16 20:53:12 -0300

    Criando o jogador.c e jogador.h

---
+++ jogadores.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5/allegro_image.h>
+
+#include "pongs.h"
