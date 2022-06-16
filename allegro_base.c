b3a9ef6fbff882837cd3432d3021a44b811ef704
Joao Gomes <joao.victor214@gmail.com>
2022-06-16 20:52:30 -0300

    Verifica teclas de movimentacao

--- allegro_base.c
+++ allegro_base.c
@@ -87,14 +87,82 @@ void verifica_esc(int *playing, ALLEGRO_EVENT ev){
 	}
 }
 
+void verifica_tecla_movimentacao(ALLEGRO_EVENT ev, Jogador *p1, Jogador *p2){
 
+	if(ev.type == ALLEGRO_EVENT_KEY_DOWN){
+		switch(ev.keyboard.keycode) {
+		//se a tecla for o W
+			case ALLEGRO_KEY_W:
+				p1->cima = 1;
+				break;
 	
+			case ALLEGRO_KEY_S:
+				p1->baixo = 1;
+				break;
 	
+			case ALLEGRO_KEY_A:
+				p1->esq = 1;
+				break;
 	
+			case ALLEGRO_KEY_D:
+				p1->dir = 1;
+				break;
+			
+			case ALLEGRO_KEY_UP:
+				p2->cima = 1;
+				break;
+			
+			case ALLEGRO_KEY_DOWN:
+				p2->baixo = 1;
+				break;
 
+			case ALLEGRO_KEY_LEFT:
+				p2->esq = 1;
+				break;
+
+			case ALLEGRO_KEY_RIGHT:
+				p2->dir = 1;
+				break;
+		}
 	}
-	
+	else if(ev.type == ALLEGRO_EVENT_KEY_UP) {
 
+		switch(ev.keyboard.keycode) {
+		//se a tecla for o W
+			case ALLEGRO_KEY_W:
+				p1->cima = 0;
+				break;
+	
+			case ALLEGRO_KEY_S:
+				p1->baixo = 0;
+				break;
+	
+			case ALLEGRO_KEY_A:
+				p1->esq = 0;
+				break;
+	
+			case ALLEGRO_KEY_D:
+				p1->dir = 0;
+				break;
+
+			case ALLEGRO_KEY_UP:
+				p2->cima = 0;
+				break;
+			
+			case ALLEGRO_KEY_DOWN:
+				p2->baixo = 0;
+				break;
+
+			case ALLEGRO_KEY_LEFT:
+				p2->esq = 0;
+				break;
+
+			case ALLEGRO_KEY_RIGHT:
+				p2->dir = 0;
+				break;
+		}
+	}		
+}
 
 int main(int argc, char **argv){
 	
@@ -195,6 +263,7 @@ int main(int argc, char **argv){
 	// FONT_32 = al_load_font("arial.ttf", 32, 1);
 
 	int playing = 1;
+
 	while(playing) {
 		ALLEGRO_EVENT ev;
 		//espera por um evento e o armazena na variavel de evento ev
@@ -225,63 +294,15 @@ int main(int argc, char **argv){
 		else if(ev.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
 			printf("\nmouse clicado em: %d, %d", ev.mouse.x, ev.mouse.y);
 		}
-		//se o tipo de evento for um pressionar de uma tecla
-		else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+		else if (ev.type == ALLEGRO_EVENT_KEY_DOWN || ev.type == ALLEGRO_EVENT_KEY_UP){
+
+			verifica_esc(&playing,ev);
+
+			verifica_tecla_movimentacao(ev,&p1,&p2);
+
 			//imprime qual tecla foi
 			// printf("\ncodigo tecla: %d", ev.keyboard.keycode);
-
-			if (ev.keyboard.keycode == ALLEGRO_KEY_ESCAPE){
-				playing = 0;
-			}
-
-			switch(ev.keyboard.keycode) {
-			//se a tecla for o W
-				case ALLEGRO_KEY_W:
-					p1.cima = 1;
-				break;
-		
-				case ALLEGRO_KEY_S:
-					p1.baixo = 1;
-				break;
-		
-				case ALLEGRO_KEY_A:
-					p1.esq = 1;
-				break;
-		
-				case ALLEGRO_KEY_D:
-					p1.dir = 1;
-				break;
-			
-			}
-
-
-			//imprime qual tecla foi
-			printf("\ncodigo tecla: %d", ev.keyboard.keycode);
 		}
-		else if(ev.type == ALLEGRO_EVENT_KEY_UP) {
-
-			switch(ev.keyboard.keycode) {
-			//se a tecla for o W
-				case ALLEGRO_KEY_W:
-					p1.cima = 0;
-				break;
-		
-				case ALLEGRO_KEY_S:
-					p1.baixo = 0;
-				break;
-		
-				case ALLEGRO_KEY_A:
-					p1.esq = 0;
-				break;
-		
-				case ALLEGRO_KEY_D:
-					p1.dir = 0;
-				break;
-
-
-			}
-		}		
-			
 
 	} //fim do while
     
