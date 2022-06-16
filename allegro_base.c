a5d74abead705bbf5b37168e5e991701f0ce4180
Joao Gomes <joao.victor214@gmail.com>
2022-06-16 20:48:26 -0300

    Criacao do pongs.h

--- allegro_base.c
+++ allegro_base.c
@@ -1,34 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_font.h>
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_primitives.h>
 #include <allegro5/allegro_image.h>
-#include <math.h>
 
+#include "pongs.h"
 
-const float FPS = 50;  
-
-const int SCREEN_W = 960;
-const int SCREEN_H = 540;
-
-//JOGADOR:
-int DIST_FUNDO = 50;
-int ALTURA_JOGADOR = 30;
-int LARGURA_JOGADOR = 100;
-float VEL_JOGADOR = 3;
-
-
-// ALLEGRO_FONT *FONT_32;
-
-
-typedef struct Jogador {
-	float x, y, h, w, vel;
-	int dir, esq, cima, baixo;
-	ALLEGRO_COLOR cor;
-	int id;
-} Jogador;
 
 
 void verifica_posicao(Jogador *p){
